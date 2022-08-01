8be5a637ef0dac9aa21ff26ca60ddddeeb555fb2
thomasfinnegan14 <finneganthomas5@gmail.com>
2022-08-01 10:39:45 -0400

    Create basic math functions

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let num1 = 1;
+let num2 = 2;
+
+function add()
+{
+    return num1 + num2;
+}
+
+function subtract()
+{
+    return num1 - num2;
+}
+
+function multiply()
+{
+    return num1 * num2;
+}
+
+function divide()
+{
+    return num1 / num2;
+}
